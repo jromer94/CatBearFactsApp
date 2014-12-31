f8f7b389194b327237aa5e16ca8ee8e8d708f2e5
Josh <jromer94@gmail.com>
2014-12-30 23:10:31 -0500

    set fragment to load fact

--- app/src/main/java/com/example/josh/catbearfacts/EditFactFragment.java
+++ app/src/main/java/com/example/josh/catbearfacts/EditFactFragment.java
@@ -6,7 +6,13 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import greendao.CatBearFact;
+import greendao.CatBearFactDao;
+import greendao.DaoSession;
 
 
 /**
@@ -15,17 +21,23 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class EditFactFragment extends Fragment {
-    private static final String ARG_FACT = "fact";
-    private static final String ARG_MESSAGE = "message";
+    private static final String ARG_FACT_ID = "id";
 
-    private String mFact;
-    private String mMessage;
 
-    public static EditFactFragment newInstance(String fact, String message) {
+    private long mId;
+
+    private CatBearFactDao mFactDao;
+    private CatBearFact mFact;
+
+    private EditText mFactEdit;
+    private EditText mMessageEdit;
+    private Button mUpdateButton;
+    private Button mDeleteButton;
+
+    public static EditFactFragment newInstance(long id) {
         EditFactFragment fragment = new EditFactFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_FACT, fact);
-        args.putString(ARG_MESSAGE, message);
+        args.putLong(ARG_FACT_ID, id);
         fragment.setArguments(args);
         return fragment;
     }
@@ -38,15 +50,44 @@ public class EditFactFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mFact = getArguments().getString(ARG_FACT);
-            mMessage = getArguments().getString(ARG_MESSAGE);
+            mId = getArguments().getLong(ARG_FACT_ID);
         }
+        DaoSession session = ((MyApplication) getActivity().getApplicationContext()).getDaoSession();
+        mFactDao = session.getCatBearFactDao();
+        mFact = mFactDao.load(mId);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_fact, container, false);
+
+        mFactEdit = (EditText) view.findViewById(R.id.edit_fact);
+        mFactEdit.setText(mFact.getName());
+
+        mMessageEdit = (EditText) view.findViewById(R.id.edit_message);
+        mMessageEdit.setText(mFact.getComment());
+
+        mUpdateButton = (Button) view.findViewById(R.id.update_button);
+        mUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               mFact.setName(mFactEdit.getText().toString());
+               mFact.setComment(mMessageEdit.getText().toString());
+               mFactDao.update(mFact);
+               //TODO return to last activity
+            }
+        });
+
+        mDeleteButton = (Button) view.findViewById(R.id.delete_button);
+        mDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFactDao.delete(mFact);
+                //TODO return to last activity
+            }
+        });
+
         return view;
     }
 
